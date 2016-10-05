9d26068da8eb6783c5aeb356e7abf4c2cf0afc40
Daine Danielson <daine@danielson.pro>
2016-10-04 22:27:15 -0700

    Clear tracks when destructing parent TrackNav.
    The implicitly defined destructor leaves an orphan track behind,forming a severe memory leak when iterating over many events

--- src/io/TrackNav.hh
+++ src/io/TrackNav.hh
@@ -14,6 +14,7 @@ public:
       { Load(ds->GetMC(), verbose); };
   TrackNav(RAT::DS::MC *mc, bool verbose=false)
       { Load(mc, verbose); };
+  ~TrackNav() { Clear(); }
   void Load(RAT::DS::MC *mc, bool verbose=false);
   void Clear();
 
